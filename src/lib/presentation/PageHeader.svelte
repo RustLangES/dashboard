e5441330cdf129f15ecadf26b020990947c18a70
Brayan-724 <bjqf0724@gmail.com>
2024-09-04 21:58:29 -0700

    feat(forms): Implement base layout

---
+++ src/lib/presentation/PageHeader.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<svelte:head>
+	<title>{title} - RustLangES Dashboard</title>
+</svelte:head>
+
+<header>
+	<h1>
+		{title}
+	</h1>
+</header>
+
+<style>
+	header {
+		width: 100%;
+		height: calc(36px + 1rem);
+
+		background: #141517;
+
+		display: flex;
+		align-items: center;
+		padding: 0 1.5rem;
+	}
+
+	h1 {
+		font-weight: bold;
+		font-size: 1.5rem;
+	}
+</style>
