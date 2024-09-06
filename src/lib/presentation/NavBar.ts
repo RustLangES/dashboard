a4ac348e242ba3c7892fe72d3b33871332bc49d3
Brayan-724 <bjqf0724@gmail.com>
2024-09-05 23:59:51 -0700

    refactor(layout): Navbar links can be specified ssr

---
+++ src/lib/presentation/NavBar.ts
@@ -0,0 +1,25 @@
+import { writable } from "svelte/store";
+
+export type NavItem =
+  | {
+    badge?: string;
+    href: string;
+    label: string;
+  }
+  | "separator";
+
+export let navbar = writable<NavItem[]>([]);
+
+export function loadNavbar(links: NavItem[]): { navbar: NavItem[] } {
+  navbar.set(links);
+
+  return {
+    navbar: links,
+  };
+}
+
+export function receiveNavbar(data: unknown) {
+  if (typeof data === "object" && data !== null && "navbar" in data) {
+    navbar.set(data.navbar as NavItem[]);
+  }
+}
