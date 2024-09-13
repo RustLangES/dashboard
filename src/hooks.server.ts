77ceb5528559092f8a1e294cd9de8a34dc9b2b97
Brayan-724 <bjqf0724@gmail.com>
2024-09-12 21:47:54 -0700

    feat: Setup Auth.js for authentication

---
+++ src/hooks.server.ts
@@ -0,0 +1,7 @@
+import { sequence } from "@sveltejs/kit/hooks";
+
+import { prepareStylesSSR } from "@svelteuidev/core";
+
+import { handle as authHandle } from "./auth";
+
+export const handle = sequence(authHandle, prepareStylesSSR);
