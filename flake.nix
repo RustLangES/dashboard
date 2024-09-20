e60827c181a29997356efea480a829c2fcf6d6f5
Brayan-724 <bjqf0724@gmail.com>
2024-09-20 00:01:15 -0700

    feat(nix): Use nodejs 22.9

--- flake.nix
+++ flake.nix
@@ -5,18 +5,34 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
     flake-utils,
     ...
-  } @ inputs:
+  }:
   # Iterate over Arm, x86 for MacOs 🍎 and Linux 🐧
     flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems) (
       system: let
-        bundle = import ./. rec {
-          inherit system flake-utils;
+        pkgs = nixpkgs.legacyPackages.${system};
 
-          pkgs = nixpkgs.legacyPackages.${system};
+        nodejs-22_9 = pkgs.stdenv.mkDerivation rec {
+          pname = "nodejs";
+          version = "22.9.0";
+
+          src = pkgs.fetchurl {
+            url = "https://nodejs.org/dist/v${version}/node-v${version}-linux-x64.tar.xz";
+            hash = "sha256-G/rp7yGrQ8ktgnTxvQMr9h9C6gBBkqGNTGRHdQhiYUI=";
+          };
+
+          installPhase = ''
+            mkdir -p $out
+
+            mv * $out/
+          '';
+
+        };
+
+        bundle = import ./. {
+          inherit system flake-utils pkgs nodejs-22_9;
         };
       in {
         inherit (bundle) apps devShells;
